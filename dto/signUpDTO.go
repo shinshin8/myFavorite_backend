8b36362f8d431e0c8fc1090e77c2766c0b69d573
sax.s.kanno.g <s.kanno.sax@gmail.com>
2019-05-03 12:29:26 +0900

    Created sign-up. #12

--- dto/signUpDTO.go
+++ dto/signUpDTO.go
@@ -1,21 +1,25 @@
 package dto
 
-// SignUpDTO struct
-type SignUpDTO struct {
-	Username     string
-	EmailAddress string
-	Password     string
+// SignUpResultJSON struct
+type SignUpResultJSON struct {
+	Status       int    `json:"status"`
+	ErrorCode    int    `json:"error_code"`
+	Username     string `json:username`
+	EmailAddress string `json:email_address`
 }
 
-// SignUpInfo create sign-up information DTO
-// Username is in the first parameter.
-// Email address is in the second parameter.
-// Password is in the third parameter.
-// It returns the instance of SignUpDTO.
-func SignUpInfo(username string, emailAddress string, password string) *SignUpDTO {
-	dto := new(SignUpDTO)
-	dto.Username = username
-	dto.EmailAddress = emailAddress
-	dto.Password = password
-	return dto
+// SignUpResult returns the pointer of struct; signUpResultJSON.
+// Status is in the first paramter.
+// ErrorCode is in the second parameter.
+// Username is in third parameter.
+// EmailAddress is in the forth parameter.
+// Password is in the fifth parameter.
+// ConfirmPassword is in the sixth parameter.
+func SignUpResult(status int, errorCode int, username string, emailAddress string) *SignUpResultJSON {
+	res := new(SignUpResultJSON)
+	res.Status = status
+	res.ErrorCode = errorCode
+	res.Username = username
+	res.EmailAddress = emailAddress
+	return res
 }
