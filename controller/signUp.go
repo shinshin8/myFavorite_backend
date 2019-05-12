b310b872c6921aad9ae5c5e6d0703327f225ee46
sax.s.kanno.g <s.kanno.sax@gmail.com>
2019-05-12 22:52:08 +0900

    created like-post model in the model package.

--- controller/signUp.go
+++ controller/signUp.go
@@ -18,120 +18,29 @@ import (
 func SignUp(w http.ResponseWriter, r *http.Request) {
 	// Input form name
 	var (
-		username        = "username"
-		emailAddress    = "emailAddress"
-		password        = "password"
-		confirmPassword = "confirmPassword"
+		usernm  = "username"
+		email   = "emailAddress"
+		pwd     = "password"
+		confPwd = "confirmPassword"
 	)
 
-	if r.Method == utils.Post {
+	// Username value
+	username := r.PostFormValue(usernm)
+	// Email address value
+	emailAddress := r.PostFormValue(email)
+	// Password value
+	password := r.PostFormValue(pwd)
+	// Confirm password value
+	confirmPassword := r.PostFormValue(confPwd)
 
-		// Username value
-		username := r.PostFormValue(username)
-		// Email address value
-		emailAddress := r.PostFormValue(emailAddress)
-		// Password value
-		password := r.PostFormValue(password)
-		// Confirm password value
-		confirmPassword := r.PostFormValue(confirmPassword)
-
-		// Validation check for username.
-		if !utils.IsName(username) {
-			// Invalid username
-			invalidUsername := 3
-			// Set values into the struct
-			resStruct := dto.SignUpResult(http.StatusOK, invalidUsername, username, emailAddress)
-			// convert struct to JSON
-			res, err := json.Marshal(resStruct)
-
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			// Set HTTP header and defined MIME type
-			w.Header().Set(utils.ContentType, utils.ApplicationJSON)
-			// Response JSON
-			w.Write(res)
-			return
-
-		}
-
-		// Validation check for email address
-		if !utils.IsEmailAddress(emailAddress) {
-			// Invalid emailAddress
-			invalidMailAddress := 4
-			// Set values into the struct
-			resStruct := dto.SignUpResult(http.StatusOK, invalidMailAddress, username, emailAddress)
-			// convert struct to JSON
-			res, err := json.Marshal(resStruct)
-
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			// Set HTTP header and defined MIME type
-			w.Header().Set(utils.ContentType, utils.ApplicationJSON)
-			// Response JSON
-			w.Write(res)
-			return
-		}
-
-		// Validation check for password
-		if !utils.IsPassword(password) {
-			// Invalid password
-			invalidPassword := 5
-			// Set values into the struct
-			resStruct := dto.SignUpResult(http.StatusOK, invalidPassword, username, emailAddress)
-
-			// convert struct to JSON
-			res, err := json.Marshal(resStruct)
-
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			// Set HTTP header and defined MIME type
-			w.Header().Set(utils.ContentType, utils.ApplicationJSON)
-			// Response JSON
-			w.Write(res)
-			return
-		}
-
-		// Check whether or not the both values: password and confrim password are equal.
-		if password != confirmPassword {
-			// Password and confirm password don't match.
-			notMatchPasswords := 6
-			// Set values into the struct
-			resStruct := dto.SignUpResult(http.StatusOK, notMatchPasswords, username, emailAddress)
-
-			// convert struct to JSON
-			res, err := json.Marshal(resStruct)
-
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			// Set HTTP header and defined MIME type
-			w.Header().Set(utils.ContentType, utils.ApplicationJSON)
-			// Response JSON
-			w.Write(res)
-			return
-		}
-
-		// Hashing password
-		hash := sha256.New()
-		hash.Write([]byte(password))
-		hexPassword := hash.Sum(nil)
-		hashedPassword := hex.EncodeToString(hexPassword)
-
-		// In this time, method returns only int; error_code.
-		signUpRes := model.SignUp(username, emailAddress, hashedPassword)
-
-		// In the Model, the function returns JSON in other way.
-		// So in this part, just response result.
-
+	// Validation check for username.
+	if !utils.IsName(username) {
+		// Invalid username
+		invalidUsername := 3
+		// Set values into the struct
+		resStruct := dto.SignUpResult(http.StatusOK, invalidUsername, username, emailAddress)
 		// convert struct to JSON
-		res, err := json.Marshal(signUpRes)
+		res, err := json.Marshal(resStruct)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -141,11 +50,36 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set(utils.ContentType, utils.ApplicationJSON)
 		// Response JSON
 		w.Write(res)
-	} else {
-		// Wrong HTTP request method
-		wrongHTTPMethod := 7
+		return
+
+	}
+
+	// Validation check for email address
+	if !utils.IsEmailAddress(emailAddress) {
+		// Invalid emailAddress
+		invalidMailAddress := 4
 		// Set values into the struct
-		resStruct := dto.SignUpResult(http.StatusOK, wrongHTTPMethod, username, emailAddress)
+		resStruct := dto.SignUpResult(http.StatusOK, invalidMailAddress, username, emailAddress)
+		// convert struct to JSON
+		res, err := json.Marshal(resStruct)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Set HTTP header and defined MIME type
+		w.Header().Set(utils.ContentType, utils.ApplicationJSON)
+		// Response JSON
+		w.Write(res)
+		return
+	}
+
+	// Validation check for password
+	if !utils.IsPassword(password) {
+		// Invalid password
+		invalidPassword := 5
+		// Set values into the struct
+		resStruct := dto.SignUpResult(http.StatusOK, invalidPassword, username, emailAddress)
 
 		// convert struct to JSON
 		res, err := json.Marshal(resStruct)
@@ -158,5 +92,52 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set(utils.ContentType, utils.ApplicationJSON)
 		// Response JSON
 		w.Write(res)
+		return
 	}
+
+	// Check whether or not the both values: password and confrim password are equal.
+	if password != confirmPassword {
+		// Password and confirm password don't match.
+		notMatchPasswords := 6
+		// Set values into the struct
+		resStruct := dto.SignUpResult(http.StatusOK, notMatchPasswords, username, emailAddress)
+
+		// convert struct to JSON
+		res, err := json.Marshal(resStruct)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Set HTTP header and defined MIME type
+		w.Header().Set(utils.ContentType, utils.ApplicationJSON)
+		// Response JSON
+		w.Write(res)
+		return
+	}
+
+	// Hashing password
+	hash := sha256.New()
+	hash.Write([]byte(password))
+	hexPassword := hash.Sum(nil)
+	hashedPassword := hex.EncodeToString(hexPassword)
+
+	// In this time, method returns only int; error_code.
+	signUpRes := model.SignUp(username, emailAddress, hashedPassword)
+
+	// In the Model, the function returns JSON in other way.
+	// So in this part, just response result.
+
+	// convert struct to JSON
+	res, err := json.Marshal(signUpRes)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// Set HTTP header and defined MIME type
+	w.Header().Set(utils.ContentType, utils.ApplicationJSON)
+	// Response JSON
+	w.Write(res)
+
 }
--- model/likedModel.go
+++ model/likedModel.go
@@ -56,3 +56,29 @@ func ShowLikedPosts(userID int) []dto.Posts {
 
 	return postArray
 }
+
+// LikePost create new like post record in MySQL and returns the result in boolean.
+// In the first parameter, user-id will be set with int type.
+// In the second paraeter, article-id will be set witn int type.
+func LikePost(userID int, articleID int) bool {
+	// Initalize DB Connection
+	sql := utils.DBInit()
+	// Close DB connection at the end.
+	defer sql.Close()
+
+	insertSyntax := `INSERT INTO 
+						liked_table
+						(user_id, 
+						article_id) 
+					VALUES
+						(?,?)`
+
+	rows, err := sql.Prepare(insertSyntax)
+
+	if err != nil {
+		return false
+	}
+
+	rows.Exec(userID, articleID)
+	return true
+}
