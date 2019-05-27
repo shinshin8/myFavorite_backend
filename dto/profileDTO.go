e064c282ef53f5264454c03db0f7d62d11254c80
sax.s.kanno.g <s.kanno.sax@gmail.com>
2019-05-27 21:38:24 +0900

    Cleaning structs

--- dto/profileDTO.go
+++ dto/profileDTO.go
@@ -4,12 +4,12 @@ package dto
 type ProfileResult struct {
 	Status    int `json:"status"`
 	ErrorCode int `json:"error_code"`
-	Profile   `json:profile`
+	Profile   `json:"profile"`
 }
 
 // Profile saves user profile data.
 type Profile struct {
-	UserID      int    `json:user_id`
+	UserID      int    `json:"user_id"`
 	UserName    string `json:"user_name"`
 	Birthday    string `json:"birthday"`
 	MailAddress string `json:"mail_address"`
